2e809e1740751b8a2d4ad63161011042e6987c33
SusanBird <susanbird391@gmail.com>
2022-06-23 17:59:47 -0600

    redirect test written and login route written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+jest.mock('../lib/services/github');
+
+describe('github oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should redirect to github auth page when log in', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
+
   afterAll(() => {
     pool.end();
   });
