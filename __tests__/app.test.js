5baa280ac5812c3c8a3dca4820158af1b6cc5766
SusanBird <susanbird391@gmail.com>
2022-06-24 17:33:11 -0600

    created mocks and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,11 +10,28 @@ describe('github oauth routes', () => {
     return setup(pool);
   });
 
-  it('should redirect to github auth page when log in', async () => {
+  it('should redirect to the github oauth page upon login', async () => {
     const res = await request(app).get('/api/v1/github/login');
+
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 
   afterAll(() => {
